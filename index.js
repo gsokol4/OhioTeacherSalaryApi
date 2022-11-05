6804db34bd45973bab5cacf12553a26ecca0a041
Gabriel Sokol <gsokol4@gmail.com>
2022-11-05 00:20:09 -0500

    working on framework for how to store data

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const res = require('express/lib/response')
 const mongoose = require('mongoose')
 require('dotenv').config()
 
@@ -12,9 +13,57 @@ mongoose.connect(
     useUnifiedTopology: true
   }
 )
+let count = 0
+const testSchema = new mongoose.Schema({ test: String })
+const testModel = new mongoose.model('testSchema', testSchema)
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
+const SchoolSalarySchema = new mongoose.Schema({
+  schoolDistrictName: [String],
+  salaries: { BA: [Number], 'BA+': [Number], 'BA++': [Number], MA: [Number], 'MA+': [Number], 'MA++': [Number], PHD: [Number] }
+})
+
+const SchoolSalaryModel = new mongoose.model('schoolDistrictSalaries', SchoolSalarySchema)
+
+app.get('/all', async (req, res) => {
+  // res.send('Hello World!')
+  let allSchools = await SchoolSalaryModel.find({})
+  console.log(allSchools)
+  res.send(allSchools)
+})
+
+app.get('/', async (req, res) => {
+  // res.send('Hello World!')
+  let allItems = await testModel.find({})
+  console.log(allItems)
+  res.send(allItems)
+})
+
+app.get('/addOne/:text', (req, res) => {
+  console.log(req.params)
+  let makeObj = (text) => { return { test: `${text}` } }
+  let obj = makeObj(req.params.text)
+  const examp = new testModel(obj)
+
+  examp.save().then(
+    (item) => {
+      console.log(item + ' has been added to the db')
+      res.send(item)
+    },
+    (err) => {
+      console.log(err)
+      res.send(err)
+    }
+  )
+})
+
+app.delete('/deleteAll', async (req, res) => {
+  try {
+    const deleted = await testModel.deleteMany({})
+    console.log(deleted)
+    res.send(`you deleted everything here is what is left ${(deleted.stringify())}`)
+  } catch (e) {
+    res.send(e)
+  }
 })
 
 app.listen(port, () => {
